b6b2f68826c59b36b86a038584daa1447ad2e3f1
Aristide <dovi.aristide@gmail.com>
2024-12-12 17:57:11 +0100

    update dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
 import { Sidebar } from "@/components/layout/sidebar";
+import { authOptions } from "@/lib/auth/auth-options";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+//import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function DashboardLayout({
   children,
